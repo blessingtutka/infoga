8713a1b23077d73610992c5ee6579ff53ac51709
blessingtutka <blessingtutka298@gmail.com>
2024-07-02 16:13:40 +0200

    test: weather api

--- api/getTemperature.js
+++ api/getTemperature.js
@@ -2,12 +2,15 @@ import axios from "axios";
 const getTemperature = async (city) => {
   const { API_KEY } = process.env;
   const END_POINT = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}`;
+  const params = {
+    origin: "*",
+  };
   try {
-    const weather = await axios.get(END_POINT);
+    const weather = await axios.get(END_POINT, params);
     const temperature = weather.data.main.temp || 11;
     return temperature;
   } catch (error) {
-    return "N/A";
+    return "N/A " + error;
   }
 };
 
